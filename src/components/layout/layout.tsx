d30ccc89e19ffb9199d7382905c45c66b54a1080
lucas-ds-domingos <lucasdomingos.ds@hotmail.com>
2025-04-01 10:43:23 -0300

    arrumando bug

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,16 +1,12 @@
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
 import  Sidebar  from "../sidebar/sidebar";
 
 export function Layout() {
-    const [selectedItem, setSelectedItem] = useState<string | null>(null);
 
     return (
-        <div style={{ display: "flex", height: "100vh" }}>
+        <div>
             <Sidebar/>
-            <div style={{ flex: 1, padding: "20px" }}>
-                <Outlet context={{ selectedItem }} /> 
-            </div>
+            <Outlet/> 
         </div>
     );
 }
